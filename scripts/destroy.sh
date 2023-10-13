06b4ceb6e74f171be5d3c2afd022eef5e5f9ade5
Corefinder89 <soumyajit.basu1988@outlook.com>
2023-10-13 14:56:45 +0530

    Mod destroy script to delete local configs

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -2,16 +2,28 @@
 
 echo "Listing all droplets"
 
-metadata=$(curl -X GET "https://api.digitalocean.com/v2/droplets" \
+metadata_ids=$(curl -X GET "https://api.digitalocean.com/v2/droplets" \
 	-H "Authorization: Bearer $DO_API_ACCESS_TOKEN")
 
 dropletids=$(echo "$metadata" | jq -r '.droplets[].id')
 
+# Delete droplets from remote [Digital ocean]
 for droplet in $dropletids; do
   curl -X DELETE \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $DO_API_ACCESS_TOKEN" \
   "https://api.digitalocean.com/v2/droplets/$droplet";
+done
+
+# Delete local configurations
+metadata_names=$(curl -X GET "https://api.digitalocean.com/v2/droplets" \
+	-H "Authorization: Bearer $DO_API_ACCESS_TOKEN")
+
+dropletnames=$(echo "$metadata" | jq -r '.droplets[].name')
+
+for droplet in $dropletnames; do
+	docker-machine rm -f droplet
+  echo deleting $droplet
 done
 
 if [ $? -eq 0 ]; then
