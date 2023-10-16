6519524b16ae6ecfb71e21dd9999695810dcc5b7
Corefinder89 <soumyajit.basu1988@outlook.com>
2023-10-16 22:03:23 +0530

    Add parallel test run code

--- python_scripts/paralleltestrun.py
+++ python_scripts/paralleltestrun.py
@@ -5,9 +5,7 @@ processes = []
 
 for counter in range(10):
     chrome_cmd = 'export BROWSER=chrome && python3 python_scripts/script1.py'
-    firefox_cmd = 'export BROWSER=firefox && python3 python_scripts/script1.py'
     processes.append(Popen(chrome_cmd, shell=True))
-    processes.append(Popen(firefox_cmd, shell=True))
 
-for counter in range(10):
+for counter in range(5):
     processes[counter].wait()
